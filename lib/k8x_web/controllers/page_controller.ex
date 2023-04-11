af7227a454c797a0f9b51483397ae9784a39d08f
Matheus Alvarenga França <matheus.alvfr@gmail.com>
2023-04-10 23:19:37 -0300

    Get POD IP

--- lib/k8x_web/controllers/page_controller.ex
+++ lib/k8x_web/controllers/page_controller.ex
@@ -2,7 +2,8 @@ defmodule K8xWeb.PageController do
   use K8xWeb, :controller
 
   def home(conn, _params) do
-    render(conn, :home)
+    host = System.get_env("POD_IP")
+    render(conn, :home, host: host)
   end
 
   def health(conn, _params) do
